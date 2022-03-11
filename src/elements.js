2626a3e7a6b0be3a35837d0b5fe9f73b2f782921
alansugito <alansugito75@gmail.com>
2022-03-11 11:37:20 +0700

    Menambahkan fitur baru

---
+++ src/elements.js
@@ -0,0 +1,19 @@
+const startButton = document.querySelector(".start-button");
+const nextButton = document.querySelector(".next-button");
+const questionContainer = document.querySelector(".question-container");
+const answerButtonContainer = document.querySelector(".answer-buttons");
+const questionElement = document.querySelector(".question");
+const exitButton = document.querySelector(".exit-button");
+const correctAudio = document.querySelector(".correct-sound");
+const wrongAudio = document.querySelector(".wrong-sound");
+const backsound = document.querySelector(".backsound");
+const login = document.querySelector(".login")
+const submitButton = document.querySelector(".btn.submit-button")
+const inputName = document.querySelector(".input-name")
+const quiz = document.querySelector(".quiz")
+const dashboard = document.querySelector(".dashboard")
+const nameTag = document.querySelector(".name")
+const correctScore = document.querySelector(".correct-score")
+const wrongScore = document.querySelector(".wrong-score")
+console.log(submitButton)
+console.log(nameTag)
---
+++ src/index.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+submitButton.addEventListener("click", logIn)
+submitButton.addEventListener("submit", logIn)
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+exitButton.addEventListener("click", exitQuiz);
+
+login.addEventListener("submit", logIn)
+})
+
+
+
